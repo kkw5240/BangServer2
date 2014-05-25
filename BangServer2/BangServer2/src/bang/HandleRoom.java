6101c77a1271b73bb8d985d8653e6870f87c5d43
kkw5240 <kkw5240@gmail.com>
2014-05-26 06:10:41 +0900

    주석 등의 띄어쓰기 수정

--- BangServer2/BangServer2/src/bang/HandleRoom.java
+++ BangServer2/BangServer2/src/bang/HandleRoom.java
@@ -5,7 +5,7 @@ public class HandleRoom implements Runnable {
 	Room r;
 	Message m;
 	
-	public HandleRoom(Room r,Message m) {
+	public HandleRoom(Room r, Message m) {
 		this.r = r;
 		this.m = m;
 	}
